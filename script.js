ead27e616df4c24112f15c239a4e524ec25598a8
unaygney <117761332+unaygney@users.noreply.github.com>
2023-06-18 16:33:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const h4Element = document.querySelector('h4');
 h4Element.addEventListener('click', function () {
 
     let redNotElements = document.querySelectorAll('.red-not');
-
+    let numberSpan = document.querySelector(".number");
+    numberSpan.style.display = "none";
     redNotElements.forEach(function (redNotElement) {
         redNotElement.style.display = 'none';
     });
